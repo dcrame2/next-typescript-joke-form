97b441ca0b1089ea3ee25bea67e606a954eb1075
Dylan Cramer <dcramer@impactnetworking.com>
2024-01-22 08:12:54 -0600

    updated form

--- src/pages/api/submitted-form.ts
+++ src/pages/api/submitted-form.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import nodemailer from "nodemailer";
-import Mail from "nodemailer/lib/mailer";
+import axios from "axios";
 
 type Data = {
   message: string;
@@ -11,6 +11,37 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  // const responseJokeData = await axios
+  //   .get("https://v2.jokeapi.dev/joke/Any", {
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Accept: "application/json",
+  //     },
+  //   })
+  //   .then((response) => console.log(response.data.joke));
+
+  let responseJokeData;
+
+  try {
+    const response = await axios.get("https://v2.jokeapi.dev/joke/Any", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+
+    if (response.data.joke) {
+      responseJokeData = response.data.joke;
+      console.log(responseJokeData); // Log the joke data
+      // Use responseJokeData elsewhere in your code
+      // For example, you can pass it to a function, display it on a webpage, etc.
+    } else {
+      console.error("No joke found in the API response");
+    }
+  } catch (error) {
+    console.error("Error fetching joke:");
+  }
+
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
@@ -28,7 +59,7 @@ export default async function handler(
     to: "dcrame2@gmail.com",
     subject: "Next/TypeScript Form",
     text: "Bro just give up on coding",
-    html: `First Name: ${req.body.enteredFirstName} <br/> Last Name: ${req.body.enteredLastName} <br/> Message: ${req.body.enteredMessage}`,
+    html: `First Name: ${req.body.enteredFirstName} <br/> Last Name: ${req.body.enteredLastName} <br/> Message: ${req.body.enteredMessage} <br /> Funny Joke: ${responseJokeData}`,
     headers: { "x-myheader": "test header" },
   });
   console.log("Message sent: %s", info.response);
